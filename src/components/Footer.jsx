e971f9641ea67194f55447734943c38a28566734
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2022-05-06 09:45:56 +0200

    Change interested from home to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,45 @@ import { ReactComponent as LogoSVG } from "../assets/logo.svg";
 import { ReactComponent as GitHubSVG } from "../assets/icons/github.svg";
 import { ReactComponent as TwitterSVG } from "../assets/icons/twitter.svg";
 import { ReactComponent as LinkedinSVG } from "../assets/icons/linkedin.svg";
+import { redirectTo } from "../utils/utils";
 
 const Footer = () => {
   return (
-    <footer id="footer">
-      <div className="cw">
-        <div className="logo">
-          <LogoSVG />
+    <>
+      <section id="interested">
+        <div className="cw">
+          <h2>Interested in doing a project together?</h2>
+          <div className="btn secondary" onClick={() => redirectTo("/contact")}>
+            Contact Me
+          </div>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">HOME</NavLink>
-            </li>
-            <li>
-              <NavLink to="portfolio">PORTFOLIO</NavLink>
-            </li>
-            <li>
-              <NavLink to="contact">CONTACT ME</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="social">
-          <GitHubSVG />
-          <TwitterSVG />
-          <LinkedinSVG />
+      </section>
+      <footer id="footer">
+        <div className="cw">
+          <div className="logo">
+            <LogoSVG />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">HOME</NavLink>
+              </li>
+              <li>
+                <NavLink to="portfolio">PORTFOLIO</NavLink>
+              </li>
+              <li>
+                <NavLink to="contact">CONTACT ME</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="social">
+            <GitHubSVG />
+            <TwitterSVG />
+            <LinkedinSVG />
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
